aca4f78e966be92b374c0bc6206ef9951497fb44
fsuke1985 <fsuke1985@users.noreply.github.com>
2023-03-10 04:47:45 +0000

    update

--- Parameters/parameter.sh
+++ Parameters/parameter.sh
@@ -1,5 +1,4 @@
 export P_NETWORK="ParameterKey=SubnetPublicCidrBlock,ParameterValue=192.168.61.128/25 ParameterKey=SubnetPrivateCidrBlock,ParameterValue=192.168.61.0/25 ParameterKey=VpcCidr,ParameterValue=192.168.61.0/24"
 
-export P_RDS="ParameterKey=DBUsername,ParameterValue=administrator ParameterKey=DBPassword,ParameterValue=sdkfj$o52143#FSS ParameterKey=DBSubnetGroupName,ParameterValue=ctvgroups ParameterKey=Subnet1Id,ParameterValue=subnet-0c818f7b73b4e5bf9 ParameterKey=Subnet2Id,ParameterValue=subnet-0d46a170e370e5052"
 
 export P_DMS="ParameterKey=SubnetId1,ParameterValue=subnet-0c818f7b73b4e5bf9 ParameterKey=SubnetId2,ParameterValue=subnet-0d46a170e370e5052 ParameterKey=AvailabilityZone,ParameterValue=ap-northeast-1a "
